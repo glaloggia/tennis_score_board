9f86b7cf4d5e59b0ffc383c4f19d440a1b62203f
glaloggia-lyit <L00169720@student.lyit.ie>
2022-08-23 20:48:24 +0100

    Improving look and feel.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tennis_score_board/dashboard.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -60,6 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        backgroundColor: Color.fromRGBO(140,45,8,1),
       ),
       body: Center(
         child: Column(
@@ -68,81 +70,102 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(children: [
               Spacer(),
             ],),
-              DataTable(
-                  border: TableBorder(verticalInside: BorderSide(width: 2),
-                      horizontalInside: BorderSide(width: 2)),
+            DataTable(
+                  headingRowColor:MaterialStateProperty.all(Color.fromRGBO(255, 111, 54, 1)),
+                  dataRowColor:MaterialStateProperty.all(Color.fromRGBO(255, 111, 54, 1)),
+
+
+                  border: TableBorder(
+                      verticalInside: BorderSide(width: 2.5,color: Color.fromRGBO(24, 217, 185, 1)),
+                      horizontalInside: BorderSide(width: 2.5,color: Color.fromRGBO(24, 217, 185, 1)),
+                      left: BorderSide(width: 5,color: Color.fromRGBO(24, 217, 185, 1)),
+                      right: BorderSide(width: 5,color: Color.fromRGBO(24, 217, 185, 1)),
+                      bottom: BorderSide(width: 5,color: Color.fromRGBO(24, 217, 185, 1)),
+                  ),
                   columnSpacing: 20,
                   columns: const [
-                DataColumn(label:Text("Player")),
-                DataColumn(label:Text("Set 1")),
-                DataColumn(label:Text("Set 2")),
-                DataColumn(label:Text("Set 3")),
-                DataColumn(label:Text("Set 4")),
-                DataColumn(label:Text("Set 5"))
-              ],
+                    DataColumn(label:Text("Player",style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontWeight: FontWeight.bold))),
+                    DataColumn(label:Text("Set 1",style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontWeight: FontWeight.bold))),
+                    DataColumn(label:Text("Set 2",style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontWeight: FontWeight.bold))),
+                    DataColumn(label:Text("Set 3",style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontWeight: FontWeight.bold))),
+                    DataColumn(label:Text("Set 4",style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontWeight: FontWeight.bold))),
+                    DataColumn(label:Text("Set 5",style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontWeight: FontWeight.bold)))
+                  ],
                   rows: [
-                DataRow(cells:[
-                  const DataCell(Text('A')),
-                  DataCell(Text(newDashboard.scoreBoard[0][0].toString())),
-                  DataCell(Text(newDashboard.scoreBoard[1][0].toString())),
-                  DataCell(Text(newDashboard.scoreBoard[2][0].toString())),
-                  DataCell(Text(newDashboard.scoreBoard[3][0].toString())),
-                  DataCell(Text(newDashboard.scoreBoard[4][0].toString())),
-                ]),
-                DataRow(cells:[
-                  const DataCell(Text('B')),
-                  DataCell(Text(newDashboard.scoreBoard[0][1].toString())),
-                  DataCell(Text(newDashboard.scoreBoard[1][1].toString())),
-                  DataCell(Text(newDashboard.scoreBoard[2][1].toString())),
-                  DataCell(Text(newDashboard.scoreBoard[3][1].toString())),
-                  DataCell(Text(newDashboard.scoreBoard[4][1].toString())),
-                ]
-                ),
-              ]
-              ),
+                  DataRow(cells:[
+                    const DataCell(Text('A',style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 40))),
+                    DataCell(Text(newDashboard.scoreBoard[0][0].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    DataCell(Text(newDashboard.scoreBoard[1][0].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    DataCell(Text(newDashboard.scoreBoard[2][0].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    DataCell(Text(newDashboard.scoreBoard[3][0].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    DataCell(Text(newDashboard.scoreBoard[4][0].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                  ]),
+                  DataRow(cells:[
+                    const DataCell(Text('B',style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 40))),
+                    DataCell(Text(newDashboard.scoreBoard[0][1].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    DataCell(Text(newDashboard.scoreBoard[1][1].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    DataCell(Text(newDashboard.scoreBoard[2][1].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    DataCell(Text(newDashboard.scoreBoard[3][1].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    DataCell(Text(newDashboard.scoreBoard[4][1].toString(),style: TextStyle(color: Color.fromRGBO(90, 90, 90, 1),fontSize: 30,fontWeight: FontWeight.bold))),
+                    ]
+                    ),
+                  ]
+            ),
             Spacer(),
+            // Divider(color: Color.fromRGBO(24, 217, 185, 1),thickness: 2,),
             Row(children: [
               Spacer(),
-              Text("Points A"),
+              Text("Points A",style: TextStyle(fontSize: 30.0,color: Colors.white,fontWeight: FontWeight.bold)),
               Spacer(),
-              Text("Points B"),
+              Text("Points B",style: TextStyle(fontSize: 30.0,color: Colors.white,fontWeight: FontWeight.bold)),
               Spacer(),
             ],),
+            Divider(color: Color.fromRGBO(24, 217, 185, 1),thickness: 5),
             Row(children: [
               Spacer(),
               Text(
                 newDashboard.currentGame[0].toString(),
-                style: Theme.of(context).textTheme.headline4,
+                style: TextStyle(fontSize: 40.0,color: Colors.white)
               ),
               Spacer(),
               Text(
                 newDashboard.currentGame[1].toString(),
-                style: Theme.of(context).textTheme.headline4,
+                style: TextStyle(fontSize: 40.0,color: Colors.white),
               ),
               Spacer()
             ],),
+            Divider(color: Color.fromRGBO(24, 217, 185, 1),thickness: 10),
             Spacer(),
             Row(children: [
               Spacer(),
-              ElevatedButton(onPressed: _incrementCounterA
-                  ,child: Text('Point to A', style: TextStyle(fontSize: 20.0),) ),
+              ElevatedButton(onPressed: _incrementCounterA,
+                  style: ButtonStyle(
+                    backgroundColor:  MaterialStateProperty.all(Color.fromRGBO(0,140,117, 1)),
+                  ),
+                  child: Text('Point to A', style: TextStyle(fontSize: 30.0),) ),
               Spacer(),
-              ElevatedButton(onPressed: _incrementCounterB
-                  ,child: Text('Point to B ', style: TextStyle(fontSize: 20.0),) ),
+              ElevatedButton(onPressed: _incrementCounterB,
+                  style: ButtonStyle(
+                    backgroundColor:  MaterialStateProperty.all(Color.fromRGBO(0,140,117,1)),
+
+                  ),
+                  child: Text('Point to B ', style: TextStyle(fontSize: 30.0),)),
               Spacer(),
             ],),
-            // Spacer(),
-            Spacer(),
             Row(children: [
               Spacer(),
-              ElevatedButton(onPressed: _resetMatch, child: Text("RESET")),
+              ElevatedButton(onPressed: _resetMatch,
+                  style: ButtonStyle(
+                    backgroundColor:  MaterialStateProperty.all(Color.fromRGBO(0,140,117, 1)),
+                  ),
+                  child: Text("RESET",style: TextStyle(fontSize: 30.0),)),
               Spacer()
             ],),
             Spacer()
           ],
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Color.fromRGBO(217, 68, 23, 1),
     );
   }
 }
